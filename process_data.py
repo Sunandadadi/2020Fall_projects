1f540dcee629b5efa80f1e178e5a8795e24d0e11
Sunandadadi <sunanda.3094@gmail>
2020-12-12 12:26:05 -0600

    Adding filtered data

---
+++ process_data.py
@@ -0,0 +1,29 @@
+# Pre processing data to fetch filtered data of required columns
+import pandas as pd
+
+# COVID-19_Case_Surveillance_Public_Use_Data_All_Months.csv contains data from CDC where onset_dt is not registered
+df = pd.read_csv('COVID-19_Case_Surveillance_Public_Use_Data_All_Months.csv', \
+    usecols = ['cdc_report_dt', 'onset_dt','current_status', 'sex', 'age_group'])
+df = df[df['onset_dt'].isnull()]
+
+df['month'] = pd.DatetimeIndex(df['cdc_report_dt']).month
+months = {
+        1: 'Jan',
+        2: 'Feb',
+        3: 'Mar',
+        4: 'Apr',
+        5: 'May',
+        6: 'Jun',
+        7: 'Jul',
+        8: 'Aug',
+        9: 'Sep',
+        10: 'Oct',
+        11: 'Nov',
+        12: 'Dec',
+    }
+
+# Building independent files for each month 
+columns = ['current_status', 'sex', 'age_group']
+for key, val in months.items():
+    temp = df.query('month=='+str(key))
+    temp.to_csv(val+'_cases.csv' , columns = columns, index = False)
